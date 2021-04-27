06c31673afbb17ab82683aed3acce7b00ff94e1a
syeda25bushra <syeda25bushra@gmail.com>
2021-04-27 19:19:16 +0530

    1

--- ai_cat_dog.py
+++ ai_cat_dog.py
@@ -1,1 +1,33 @@
-x=0
+# working with, mainly resizing, imag
+import cv2   
+import numpy as np         # dealing with arrays
+import os                  # dealing with directories
+from random import shuffle # mixing up or currently ordered data that might lead our network astray in training.
+from tqdm import tqdm      # a nice pretty percentage bar for tasks. Thanks to viewer Daniel BA1/4hler for this suggestion
+
+TRAIN_DIR ='D:\data for catdog ai\dog vs cat\dataset\training_set'
+TEST_DIR ='D:\data for catdog ai\dog vs cat\dataset\test_set'
+
+
+IMG_SIZE = 50
+LR = 1e-3
+
+MODEL_NAME = 'dogsvscats-{}-{}.model'.format(LR, '2conv-basic') # just so we remember which saved model is which, sizes must match
+def label_img(img):
+    word_label = img.split('.')[-3]
+    # conversion to one-hot array [cat,dog]
+    #                            [much cat, no dog]
+    if word_label == 'cat': return [1,0]
+    #                             [no cat, very doggo]
+    elif word_label == 'dog': return [0,1]
+def create_train_data():
+    training_data = []
+    for img in tqdm(os.listdir(TRAIN_DIR)):
+        label = label_img(img)
+        path = os.path.join(TRAIN_DIR,img)
+        img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
+        training_data.append([np.array(img),np.array(label)])
+    shuffle(training_data)
+    np.save('train_data.npy', training_data)
+    return training_data
